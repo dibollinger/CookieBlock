2058a19b94756a86ce9fd9792a904fb2bd20a567
Dino Bollinger <dino.bollinger@inf.ethz.ch>
2021-05-16 12:08:29 +0200

    Add mockup for cookie config. Not functional yet.

---
+++ src/options/cookieblock_cconfig.js
@@ -0,0 +1,13 @@
+
+
+const setupConfigPage = function() {
+    setStaticLocaleText("cconfig-title", "extensionName");
+    setStaticLocaleText("cconfig-subtitle", "cookieConfigSubtitle");
+    setStaticLocaleText("cconfig-desc-title", "cookieConfigDescTitle");
+    setStaticLocaleText("cconfig-desc-pg1", "cookieConfigDescPG1");
+    setStaticLocaleText("cconfig-desc-pg2", "cookieConfigDescPG2");
+    setStaticLocaleText("cconfig-list-title", "cookieConfigListTitle");
+}
+
+
+document.addEventListener("DOMContentLoaded", setupConfigPage);
