28b69c36f1bb884489a8c26cae68305a5986acdd
Dino Bollinger <dino.bollinger@gmail.com>
2022-08-04 13:05:02 +0200

    Node Extractor: Use a hack to read big json files asynchronously.

--- node-feature-extractor/modules/utils.js
+++ node-feature-extractor/modules/utils.js
@@ -9,6 +9,8 @@ Released under the MIT License, see included LICENSE file.
 //-------------------------------------------------------------------------------
 
 let fs = require('fs');
+let Stream = require('stream');
+let JSONStream = require('JSONStream');
 
 /**
 * Retrieves the data at the given URL with the specified type. Only intended for local extension data access.
@@ -21,8 +23,33 @@ const getLocalData = function(url, dtype, callback) {
     let read_data;
     try {
         if (dtype === "json"){
+            read_data = undefined;
+            let readingData = true;
             let raw_data = fs.readFileSync(url);
-            read_data = JSON.parse(raw_data);
+            var s = new Stream();
+            s.pipe = function(dest) {
+              dest.write(raw_data);
+              return dest;
+            };
+
+            var parser = JSONStream.parse();
+            parser.on('data', function(obj) {
+                read_data = obj;
+                readingData = false;
+            });
+
+            parser.on('error', function(err) {
+                console.trace(err.message);
+                readingData = false;
+            });
+
+            // read data, and makeshift synchronous wait
+            s.pipe(parser);
+            while (readingData);
+
+            // bail out if still undefine
+            if (read_data == undefined)
+                return 1;
         } else if (dtype === "text"){
             read_data = fs.readFileSync(url, "utf-8");
         } else{
@@ -30,7 +57,7 @@ const getLocalData = function(url, dtype, callback) {
         }
         callback(read_data);
     } catch (err) {
-        console.error(err.message);
+        console.error(err.stack);
         return 1;
     }
     return 0;
